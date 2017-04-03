086f4de49f22c46a6b569cd305417765a7ab7d91
Christophe THIERRY <christophe@host.dynamic.voo.be>
2017-04-04 01:01:29 +0200

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Christophe THIERRY on 4/04/17.
+//  Copyright © 2017 Christophe THIERRY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
